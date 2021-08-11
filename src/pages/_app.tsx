ba0d471f8a1804ac4a33c840eddb01d33a443c19
smarulanda97 <santiagomm1997@gmail.com>
2021-08-11 00:45:06 -0500

    Setup react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,21 @@
+import React from "react";
+import type { AppProps } from 'next/app';
+import { Hydrate } from "react-query/hydration";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from "react-query";
+
 import '@styles/index.scss';
-import type { AppProps } from 'next/app'
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+  )
 }
 export default App;
