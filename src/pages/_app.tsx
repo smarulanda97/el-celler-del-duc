a1dcebf34b7d065d1b8960a11996099a2832194d
smarulanda97 <santiagomm1997@gmail.com>
2021-08-19 02:32:20 -0500

    Styles for header min width lg

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,27 @@
+import React from 'react';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
-import React, { ReactElement } from 'react';
 import { Hydrate } from 'react-query/hydration';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { queryClient, QueryClientProvider } from '@libs/react-query/client';
 
 import '@styles/index.scss';
 
-function App({ Component, pageProps }: AppProps): ReactElement {
+function App({ Component, pageProps }: AppProps): React.ReactElement {
   const [client] = React.useState(() => queryClient);
 
   return (
-    <QueryClientProvider client={client}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
-      </Hydrate>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <React.Fragment>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <QueryClientProvider client={client}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </React.Fragment>
   );
 }
 export default App;
