7fe7e135e490a7fedfbbfbe98d5945f43ed68030
Santiago Marulanda <santiagomm1997@gmail.com>
2021-09-22 23:22:27 -0500

    Styles for Testimonials component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,600;0,700;1,400;1,700&family=Great+Vibes&family=Libre+Baskerville:ital@0;1&family=Open+Sans:ital,wght@0,600;0,700;1,600;1,700&family=Oswald:wght@300;400;700&family=Yellowtail&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,600;0,700;1,400;1,700&family=Great+Vibes&family=Libre+Baskerville:ital@0;1&family=Open+Sans:ital,wght@0,600;0,700;1,600;1,700&family=Oswald:wght@300;400;700&family=Yellowtail&family=Bitter:wght@700&display=swap"
             rel="stylesheet"
           />
         </Head>
